bbe50fa91d52212449e0d0eac0fe95f9b26bb21d
Arman <armanmahal2004@gmail.com>
2024-06-25 13:52:21 +0530

    setup redux store and create user slice

---
+++ client/src/redux/user/userSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: null,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    signInSuccess: (state, action) => {
+      state.user = action.payload;
+    },
+  },
+});
+
+export const { signInStart, signInSuccess, signInFailure } = userSlice.actions;
+
+export default userSlice.reducer;
