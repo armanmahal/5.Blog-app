9d993e22a190d2d5b42ebe8f27aadee36ba2874b
Arman <armanmahal2004@gmail.com>
2024-06-29 10:40:42 +0530

    create api route to update user password

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/userModel.js";
+import bcryptjs from "bcryptjs";
 
 export const test = (req, res) => {
   res.json({ message: "working" });
@@ -18,13 +19,45 @@ export const updateImage = async (req, res) => {
       { new: true }
     );
 
-    res
-      .status(200)
-      .json({
-        success: true,
-        message: "Image Updated successfully",
-        newImageUrl: UpdatedUser.image,
-      });
+    res.status(200).json({
+      success: true,
+      message: "Image Updated successfully",
+      newImageUrl: UpdatedUser.image,
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+export const updatePassword = async (req, res) => {
+  if (req.params.userId !== req.user.id) {
+    return res.status(401).json({ success: false, message: "Unauthorized" });
+  }
+
+  //if invalid input:
+  if (req.body.password === "") {
+    return res
+      .status(400)
+      .json({ success: false, message: "Enter a Valid Password" });
+  }
+
+  try {
+    const hashedPass = bcryptjs.hashSync(req.body.password, 10);
+
+    const UpdatedUser = await User.findByIdAndUpdate(
+      req.user.id,
+      {
+        password: hashedPass,
+      },
+      { new: true }
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "Password Updated successfully",
+    });
   } catch (error) {
     return res
       .status(500)
--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { test, updateImage } from "../controllers/userController.js";
+import {
+  test,
+  updateImage,
+  updatePassword,
+} from "../controllers/userController.js";
 import { verifyUser } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.get("/test", test);
 router.put("/updateImage/:userId", verifyUser, updateImage);
+router.put("/updatePassword/:userId", verifyUser, updatePassword);
 
 export default router;
