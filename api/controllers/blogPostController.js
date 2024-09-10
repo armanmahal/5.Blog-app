8d70c17ee530f0b4d79f08fe93d6cab8fe202bae
Arman <armanmahal2004@gmail.com>
2024-09-10 09:03:56 +0530

    complete get posts api route

--- api/controllers/blogPostController.js
+++ api/controllers/blogPostController.js
@@ -36,3 +36,25 @@ export const create = async (req, res) => {
     res.status(500).json({ success: false, message: "Internal Server Error" });
   }
 };
+
+export const getPosts = async (req, res) => {
+  // if not admin:
+  if (!req.user.isAdmin) {
+    return res.status(401).json({ success: false, message: "Unauthorized" });
+  }
+
+  const limit = req.body.limit || 10;
+  const startIndex = req.body.startIndex * limit || 0;
+
+  try {
+    const posts = await Post.find()
+                            .sort({'createdAt' : -1}) // 1 for ascending, -1 for descending.
+                            .skip(startIndex)
+                            .limit(limit);
+                            
+
+    res.status(200).json({success: true, posts});
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
--- api/routes/blogPostRoute.js
+++ api/routes/blogPostRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { create } from "../controllers/blogPostController.js";
+import { create, getPosts } from "../controllers/blogPostController.js";
 import { verifyUser } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post("/create", verifyUser, create);
+router.get("/getposts", verifyUser, getPosts);
 
 export default router;
